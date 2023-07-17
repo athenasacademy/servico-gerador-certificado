0a22bde9b0b8615f7351aee7764cb278ca43d007
athenasacademy <athenasacademy.flow@gmail.com>
2023-07-17 11:40:39 -0300

    add CertificadoRequest

--- src/AthenasAcademy.GeradorCertificado.Dominio/Requests/CertificadoRequest.cs
+++ src/AthenasAcademy.GeradorCertificado.Dominio/Requests/CertificadoRequest.cs
@@ -8,5 +8,14 @@ namespace AthenasAcademy.GeradorCertificado.Dominio.Requests
 {
     public class CertificadoRequest
     {
+        public string NomeAluno { get; set; }
+
+        public int Matricula { get; set; }
+
+        public int Aproveitamento { get; set; }
+
+        public string NomeCurso { get; set; }
+
+        public int CargaHoraria { get; set; }
     }
 }
