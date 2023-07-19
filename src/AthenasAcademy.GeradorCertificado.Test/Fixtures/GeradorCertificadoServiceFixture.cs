9c5200cd6b115fd70cbf23b0663dfe62f946ea98
athenasacademy <athenasacademy.flow@gmail.com>
2023-07-18 22:23:08 -0300

    Add testes unitarios

---
+++ src/AthenasAcademy.GeradorCertificado.Test/Fixtures/GeradorCertificadoServiceFixture.cs
@@ -0,0 +1,141 @@
+﻿using AthenasAcademy.GeradorCertificado.Dominio.Requests;
+using AthenasAcademy.GeradorCertificado.Dominio.Responses;
+using AthenasAcademy.GeradorCertificado.Models;
+using AthenasAcademy.GeradorCertificado.Repositories.Interfaces;
+using AthenasAcademy.GeradorCertificado.Services;
+using AthenasAcademy.GeradorCertificado.Services.Interfaces;
+using AutoFixture;
+using NSubstitute;
+using System.Collections.Generic;
+
+namespace AthenasAcademy.GeradorCertificado.Test.Fixtures
+{
+    public class GeradorCertificadoServiceFixture
+    {
+        #region Dependencias
+        private Fixture _fixture;
+
+        private IGerenciadorArquivosService gerenciadorArquivosService;
+        private IQRCodeService qrCodeService;
+        private IPNGService pngService;
+        private IAwsS3Repository awsS3Repository;
+        #endregion
+
+        #region Construtores
+        public GeradorCertificadoServiceFixture()
+        {
+            _fixture = new Fixture();
+
+            gerenciadorArquivosService = Substitute.For<IGerenciadorArquivosService>();
+            qrCodeService = Substitute.For<IQRCodeService>();
+            pngService = Substitute.For<IPNGService>();
+            awsS3Repository = Substitute.For<IAwsS3Repository>();
+        }
+
+        public IGeradorCertificadoService InstanciarServico()
+        {
+            return GeradorCertificadoService.Instancia;
+        }
+        #endregion
+
+        #region Calls
+        public IEnumerable<NSubstitute.Core.ICall> GetGerenciadorArquivosServiceCalls()
+        {
+            return gerenciadorArquivosService.ReceivedCalls();
+        }
+
+        public IEnumerable<NSubstitute.Core.ICall> GetQRCodeServiceCalls()
+        {
+            return qrCodeService.ReceivedCalls();
+        }
+
+        public IEnumerable<NSubstitute.Core.ICall> GetPNGServiceCalls()
+        {
+            return pngService.ReceivedCalls();
+        }
+
+        public IEnumerable<NSubstitute.Core.ICall> GetAwsS3RepositoryCalls()
+        {
+            return awsS3Repository.ReceivedCalls();
+        }
+        #endregion
+
+        #region Aranges
+        public GeradorCertificadoServiceFixture ComGerarQRCodeOK()
+        {
+            qrCodeService.GerarQRCode(
+                Arg.Any<string>(),
+                Arg.Any<int>(),
+                Arg.Any<int>(),
+                Arg.Any<string>()
+                ).
+                ReturnsForAnyArgs(
+                _fixture.Create<QrCodeDetalhesModel>()
+                );
+
+            return this;
+        }
+
+        public GeradorCertificadoServiceFixture ComGerarPNGOK()
+        {
+            pngService.GerarPNG(
+                Arg.Any<string>(),
+                Arg.Any<string>(),
+                Arg.Any<string>(),
+                Arg.Any<string>(),
+                Arg.Any<string>()
+                ).
+                ReturnsForAnyArgs(
+                _fixture.Create<PNGDetalhesModel>()
+                );
+
+            return this;
+        }
+
+        public GeradorCertificadoServiceFixture ComEnviarPDFAsyncOK()
+        {
+            awsS3Repository.EnviarPDFAsync(
+                Arg.Any<PDFDetalhesModel>(),
+                Arg.Any<string>()
+                ).
+                ReturnsForAnyArgs(
+                Arg.Any<string>()
+                );
+
+            return this;
+        }
+
+        public GeradorCertificadoServiceFixture ComRecuperarBytesArquivoAsyncOK()
+        {
+            gerenciadorArquivosService.RecuperarBytesArquivoAsync(
+                Arg.Any<string>()
+                ).
+                ReturnsForAnyArgs(
+                Arg.Any<byte[]>()
+                );
+
+            return this;
+        }
+
+        public GeradorCertificadoServiceFixture ComGerarURIAsyncOK()
+        {
+            awsS3Repository.GerarURIAsync(
+                Arg.Any<string>(),
+                Arg.Any<string>()
+                ).
+                ReturnsForAnyArgs(
+                Arg.Any<string>()
+                );
+
+            return this;
+        }
+        #endregion
+
+        #region Mocks
+        public NovoCertificadoRequest NovoCertificadoRequestMOCK()
+        {
+            return _fixture.Create<NovoCertificadoRequest>();
+        }
+        #endregion
+    }
+}
--- src/AthenasAcademy.GeradorCertificado.Test/Testes/GeradorCertificadoServiceTeste.cs
+++ src/AthenasAcademy.GeradorCertificado.Test/Testes/GeradorCertificadoServiceTeste.cs
@@ -1,13 +1,57 @@
-﻿using System;
+﻿using AthenasAcademy.GeradorCertificado.Dominio.Requests;
+using AthenasAcademy.GeradorCertificado.Dominio.Responses;
+using AthenasAcademy.GeradorCertificado.Services.Interfaces;
+using AthenasAcademy.GeradorCertificado.Test.Fixtures;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute.Core;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace AthenasAcademy.GeradorCertificado.Test.Testes
 {
     public class GeradorCertificadoServiceTeste
     {
+        [Fact]
+        public async Task DeveGerarPDF()
+        {
+            // Arange
+            GeradorCertificadoServiceFixture fixture = new GeradorCertificadoServiceFixture();
+            IGeradorCertificadoService service = fixture
+                                                .ComGerarQRCodeOK()
+                                                .ComGerarPNGOK()
+                                                .ComEnviarPDFAsyncOK()
+                                                .ComRecuperarBytesArquivoAsyncOK()
+                                                .ComGerarURIAsyncOK()
+                                                .InstanciarServico();
 
+            // Act
+            NovoCertificadoRequest request = fixture.NovoCertificadoRequestMOCK();
+            NovoCertificadoPDFResponse response = await service.GerarCertificadoPDF(request);
+
+            IEnumerable<ICall> callsArquivoService = fixture.GetGerenciadorArquivosServiceCalls()
+                .Where(x => 
+                       x.GetMethodInfo().Name == "LimparCaminhoBase" && 
+                       x.GetMethodInfo().Name == "RecuperarBytesArquivoAsync");
+
+            IEnumerable<ICall> callsQRCodeService = fixture.GetQRCodeServiceCalls()
+                .Where(x => x.GetMethodInfo().Name == "GerarQRCode");
+
+            IEnumerable<ICall> callsPNGService = fixture.GetPNGServiceCalls()
+                .Where(x => x.GetMethodInfo().Name == "TryGetValue");
+
+            IEnumerable<ICall> callsAwsS3Repository = fixture.GetAwsS3RepositoryCalls()
+                .Where(x => 
+                       x.GetMethodInfo().Name == "EnviarPDFAsync" &&
+                       x.GetMethodInfo().Name == "GerarURIAsync");
+
+            // Assert
+            Assert.Equals(2, callsArquivoService.Count());
+            Assert.Equals(1, callsQRCodeService.Count());
+            Assert.Equals(1, callsPNGService.Count());
+            Assert.Equals(2, callsAwsS3Repository.Count());
+            Assert.IsNotNull(response);
+        }
     }
 }
