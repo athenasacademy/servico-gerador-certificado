2aca77e25d44a2978bb13e6d2c8cbfb9103babc3
Gabriel Alexandre <gabriel.alexandre.dev@gmail.com>
2023-07-20 23:49:58 -0300

    Adicao Estrutura para ler secrets no environment yaml

---
+++ src/AthenasAcademy.GeradorCertificado/Models/SecretConfig.cs
@@ -0,0 +1,18 @@
+﻿
+using PdfSharp.Charting;
+using YamlDotNet.Serialization;
+
+namespace AthenasAcademy.GeradorCertificado.Models
+{
+    public class SecretConfig
+    {
+        [YamlMember(Alias = "AwsAccessKey")]
+        public string AwsAccessKey { get; set; }
+
+        [YamlMember(Alias = "AwsSecretKey")]
+        public string AwsSecretKey { get; set; }
+
+        [YamlMember(Alias = "AwsBucketBase")]
+        public string AwsBucketBase { get; set; }
+    }
+}
